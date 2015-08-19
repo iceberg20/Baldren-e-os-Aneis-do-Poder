9440cb5a24d59a5220e1ffd6975f6e50cf684b39
Italo Berg <italoberg@yahoo.com.br>
2015-08-19 14:57:52 -0300

    Colisão implementada
    -Apenas mostra um Debug.Log("Colidiu")

---
+++ Assets/_Scripts/EnemyColision.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyColision : MonoBehaviour {
+	public BoxCollider2D inimigo;
+	public BoxCollider2D heroi;
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(inimigo.IsTouching(heroi))
+		   Debug.Log("Colidiu");	
+		   
+	}
+
+
+
+}
