4219b14fb5d1771552dc3d6e1e72504f528819bb
Italo Berg <italoberg@yahoo.com.br>
2015-08-28 08:18:46 -0300

    Tutorial passo 2 - Spikes

--- Assets/Tutor.cs
+++ Assets/Tutor.cs
@@ -6,6 +6,7 @@ public class Tutor : MonoBehaviour {
 	public Text text;
 	public Transform heroi;
 	private bool step1;
+	private bool step2;
 	public BoxCollider2D step1Coll;
 
 	// Use this for initialization
@@ -17,7 +18,7 @@ public class Tutor : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		//Debug.Log(heroi.transform.position.x);
+		Debug.Log(heroi.transform.position.x);
 		if(heroi.transform.position.x >=-7.0f && !step1){
 			text.text = "Aperte para cima para pular!";
 			step1= true;
@@ -25,6 +26,14 @@ public class Tutor : MonoBehaviour {
 
 
 		if(Input.GetKeyDown(KeyCode.UpArrow) && step1)
+			text.text = "";
+
+		if(heroi.transform.position.x >=-4.8f && !step2){
+			text.text = "Cuidado com os espinhos!";
+			step2= true;		}
+		
+		
+		if(Input.GetKeyDown(KeyCode.UpArrow) && step2)
 			text.text = "";
 	
 	}
