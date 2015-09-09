383805c57ff5d40a05843b6ff691242fd845809d
Italo Berg <italoberg@yahoo.com.br>
2015-09-08 23:48:47 -0300

    Dragon Fire ball

---
+++ Assets/DragonBoss.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragonBoss : MonoBehaviour {
+	private float time = 0f;
+	public Transform fireBall;
+	private Transform boos;
+
+	// Use this for initialization
+	void Start () {
+		boos = gameObject.GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time+= Time.deltaTime;
+		if(time>1f){
+			Instantiate(fireBall,new Vector3(boos.position.x-0.5f, boos.position.y-0.1f,1f),Quaternion.identity);
+			time =0;
+		}
+	
+	}
+}
---
+++ Assets/FireBalll.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireBalll : MonoBehaviour {
+	private Transform t;
+
+	// Use this for initialization
+	void Start () {
+		t = gameObject.GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		t.Translate(Vector3.left* Time.deltaTime);
+	}
+}
