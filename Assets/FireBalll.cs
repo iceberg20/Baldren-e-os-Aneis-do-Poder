0c165a2f2229913c22479fb335645b295bf42cd7
Italo Berg <italoberg@yahoo.com.br>
2015-09-10 11:37:20 -0300

    Fire ball collision - Roack wall bug fix
    -The fire ball is destroyed when hit the hero
    -Rock wall is not pulled when hited by the fireball

--- Assets/FireBalll.cs
+++ Assets/FireBalll.cs
@@ -3,14 +3,31 @@ using System.Collections;
 
 public class FireBalll : MonoBehaviour {
 	private Transform t;
+	private BoxCollider2D heroColl;
+	private GameObject hero;
+	private BoxCollider2D fireBall;
+	public GameObject fb;
 
-	// Use this for initialization
+
+	// Use this for initialization	
 	void Start () {
 		t = gameObject.GetComponent<Transform>();
+		fireBall = gameObject.GetComponent<BoxCollider2D>();
+		hero = GameObject.FindGameObjectWithTag("Player");
+		heroColl = hero.GetComponent<BoxCollider2D>();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		t.Translate(Vector3.left* (Time.deltaTime*3) );
+
+		if(fireBall.IsTouching(heroColl)){
+			Destroy(fb);
+		}
+
+		if(t.position.x<11.6f)
+			Destroy(fb);
+
 	}
 }
